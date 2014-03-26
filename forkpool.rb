e034cad3411a4eb97348443f74735fb874a123cd
Martin Asser Hansen <mail@maasha.dk>
2014-03-26 22:08:17 +0100

    Rewrote code complete to an Enumerable monkey patch. Hangs occationally (eof) and suffers from race conditions.

--- forkpool.rb
+++ forkpool.rb
@@ -1,105 +1,102 @@
 #!/usr/bin/env ruby
- 
+
 require 'pp'
 
-DEBUG = 1
- 
-# Class to setup a fork pool with a specified _count_ of workers to process in
-# parallel incomming jobs; potentially many more jobs than _count_. The results
-# are returned in order they are given to the workers.
-#
-# ForkPool.new(count) |workers| do
-#   heavy_tasks.each do |task|
-#       result = workers.process task
-#     end
-#   end
-# end
-class ForkPool
-  # Method to construct a new ForkPool object given a _count_ of workers to be
-  # spawned.
-  def initialize(count)
-    $stderr.puts "Parent pid: #{Process.pid}"
+DEBUG = false
+CPUS  = 2
 
-    @parent  = true
-    @workers = spawn_workers(count)
-    @tickets = Array.new(count, false)
+module Enumerable
+  # Fork each (feach) creates a fork pool with a specified number of processes
+  # (_procs_) to iterate over the Enumerable object processing the specified
+  # block. Calling feach with 0 _procs_ disables forking for debugging purposes.
+  #
+  # @example - process 10 elements using 4 processes:
+  #
+  # (0 ... 10).feach(4) { |i| puts i; sleep 1 }
+  def feach(procs, &block)
+    $stderr.puts "Parent pid: #{Process.pid}" if DEBUG
 
-    yield self
-  end
- 
-  # Method to process a _block_ in a parallel process and return the results.
-  # @tickets contain information about idle workers; we find one and deletate
-  # the processing to this worker.
-  def process(&block)
-    @tickets.each_with_index do |ticket, index|
-      if ! ticket
-        worker = @workers[index]
-        @tickets[index] = true
+    if procs > 0
+      workers = spawn_workers(procs, &block)
+      threads = []
 
-        $stderr.puts "Worker #{index} with pid: #{worker.pid} put to work" if DEBUG
+      self.each_with_index do |elem, index|
+        $stderr.puts "elem: #{elem}    index: #{index}" if DEBUG
 
-        worker.process(&block)
-        @tickets[index] = false
- 
-        return Marshal.load(worker.ipc_read)
+        threads << Thread.new do 
+          worker = workers[index % procs]
+          worker.process(elem)
+        end
+      end
+
+      threads.each { |thread| thread.join }
+      workers.each { |worker| worker.terminate }
+    else
+      self.each do |elem|
+        block.call(elem)
       end
     end
   end
- 
-  private
- 
-  # Method to spawn a given _count_ of workers that are returned on an array.
-  # Each worker have an IO pipe for Inter Process Communication (IPC) of
-  # results from the worker to the parent.
-  def spawn_workers(count)
+
+  def spawn_workers(procs, &block)
     workers = []
- 
-    (0 ... count).each do
-      ipc_read, ipc_write = IO.pipe
- 
+
+    procs.times do 
+      child_read, parent_write = IO.pipe
+      parent_read, child_write = IO.pipe
+
       pid = Process.fork do
-        @parent = false
+        begin
+          parent_write.close
+          parent_read.close
+          call(child_read, child_write, &block)
+        ensure
+          child_read.close
+          child_write.close
+        end
       end
 
-      $stderr.puts "Spawning worker with pid: #{pid} from parent process: #{Process.pid}" if DEBUG
- 
-      workers << Worker.new(ipc_read, ipc_write, pid) if @parent
+      $stderr.puts "Spawning worker with pid: #{pid}" if DEBUG
+
+      workers << Worker.new(parent_read, parent_write, pid)
     end
- 
+
     workers
   end
- 
-  # Class with methods to construct and manipulate Worker objects.
+
+  def call(child_read, child_write, &block)
+    while not child_read.eof?
+      elem = Marshal.load(child_read)
+      $stderr.puts "      call with Process.pid: #{Process.pid}" if DEBUG
+      result = block.call(elem)
+      Marshal.dump(result, child_write)
+    end
+  end
+
   class Worker
-    attr_reader :ipc_read, :ipc_write, :pid
- 
-    def initialize(ipc_read, ipc_write, pid)
-      @ipc_read  = ipc_read
-      @ipc_write = ipc_write
-      @pid       = pid
-    end
- 
-    # Method to process a given block and dump to the result IPC pipe.
-    def process(&block)
-      result = block.call
+    attr_reader :parent_read, :parent_write, :pid
 
-      $stderr.puts "Worker with pid: #{@pid} at work! Working process is #{Process.pid}" if DEBUG
- 
-      Marshal.dump(result, @ipc_write)
+    def initialize(parent_read, parent_write, pid)
+      @parent_read  = parent_read
+      @parent_write = parent_write
+      @pid          = pid
+    end
+
+    def process(elem)
+      Marshal.dump(elem, @parent_write)
+      $stderr.puts "   process with worker pid: #{@pid} and parent pid: #{Process.pid}" if DEBUG
+      Marshal.load(@parent_read)
+    end
+
+    def terminate
+      $stderr.puts "Terminating worker with pid: #{@pid}" if DEBUG
+      @parent_read.close
+      @parent_write.close
+      Process.wait(@pid, Process::WNOHANG)
     end
   end
 end
- 
-# Example processing 100 heavy jobs using 4 processes:
- 
+
 def fib(n) n < 2 ? n : fib(n-1)+fib(n-2); end # Lousy Fibonacci calculator <- heavy job
- 
-ForkPool.new(14) do |workers|
-  (0 .. 100).each do |i|
-    $stderr.puts "Sending job #{i} for processing" if DEBUG
 
-    result = workers.process { "Job: #{i} - Calculating fib(35) = " + fib(35).to_s }
- 
-    pp result
-  end
-end
+(0 ... 20).feach(CPUS) { |i| puts "#{i}: #{fib(Random.rand(20..35))}" }
