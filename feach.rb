620d04ef0fcd104448fc8fcbb6e87f3239759119
Martin Asser Hansen <mail@maasha.dk>
2014-03-28 17:25:18 +0100

    fixed feach cache

--- feach.rb
+++ feach.rb
@@ -2,8 +2,7 @@
 
 require 'pp'
 
-DEBUG = false
-CPUS  = 2
+FEACH_DEBUG = false
 
 module Enumerable
   # Fork each (feach) creates a fork pool with a specified number of processes
@@ -16,7 +15,7 @@ module Enumerable
   #
   # (0 ... 10).feach(:processes => 4) { |i| puts i; sleep 1 }
   def feach(options = {}, &block)
-    $stderr.puts "Parent pid: #{Process.pid}" if DEBUG
+    $stderr.puts "Parent pid: #{Process.pid}" if FEACH_DEBUG
 
     procs = options[:processes]   || 0
     sync  = options[:synchronize] || true
@@ -24,22 +23,27 @@ module Enumerable
     if procs > 0
       workers = spawn_workers(procs, &block)
       threads = []
+      cache   = []
 
       self.each_with_index do |elem, index|
-        $stderr.puts "elem: #{elem}    index: #{index}" if DEBUG
+        $stderr.puts "elem: #{elem}    index: #{index}" if FEACH_DEBUG
 
         threads << Thread.new do 
-          worker = workers[index % procs]
-          worker.process(elem)
+          i = index % procs
+          worker   = workers[i]
+          cache[i] = worker.process(elem)
         end
 
         if threads.size == procs
           threads.each { |thread| thread.join }
+          cache.each { |result| puts result }
           threads = []
+          cache   = []
         end
       end
 
       threads.each { |thread| thread.join }
+      cache.each { |result| puts result }
       workers.each { |worker| worker.terminate }
     else
       self.each do |elem|
@@ -69,7 +73,7 @@ module Enumerable
       child_read.close
       child_write.close
 
-      $stderr.puts "Spawning worker with pid: #{pid}" if DEBUG
+      $stderr.puts "Spawning worker with pid: #{pid}" if FEACH_DEBUG
 
       workers << Worker.new(parent_read, parent_write, pid)
     end
@@ -80,7 +84,7 @@ module Enumerable
   def call(child_read, child_write, &block)
     while not child_read.eof?
       elem = Marshal.load(child_read)
-      $stderr.puts "      call with Process.pid: #{Process.pid}" if DEBUG
+      $stderr.puts "      call with Process.pid: #{Process.pid}" if FEACH_DEBUG
       result = block.call(elem)
       Marshal.dump(result, child_write)
     end
@@ -97,12 +101,12 @@ module Enumerable
 
     def process(elem)
       Marshal.dump(elem, @parent_write)
-      $stderr.puts "   process with worker pid: #{@pid} and parent pid: #{Process.pid}" if DEBUG
+      $stderr.puts "   process with worker pid: #{@pid} and parent pid: #{Process.pid}" if FEACH_DEBUG
       Marshal.load(@parent_read)
     end
 
     def terminate
-      $stderr.puts "Terminating worker with pid: #{@pid}" if DEBUG
+      $stderr.puts "Terminating worker with pid: #{@pid}" if FEACH_DEBUG
       Process.wait(@pid, Process::WNOHANG)
       @parent_read.close
       @parent_write.close
@@ -112,4 +116,4 @@ end
 
 def fib(n) n < 2 ? n : fib(n-1)+fib(n-2); end # Lousy Fibonacci calculator <- heavy job
 
-(0 ... 10).feach(processes: CPUS) { |i| puts "#{i}: #{fib(35)}" }
+(0 ... 10).feach(processes: 1) { |i| "#{i}: #{fib(35)}" }
