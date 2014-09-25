83cc226785406cce193d7f3397c4599fec52f6b7
Martin Asser Hansen <mail@maasha.dk>
2014-09-25 10:38:27 +0200

    miseq_demultiplexer now working, but slow

--- miseq_demultiplexer.rb
+++ miseq_demultiplexer.rb
@@ -5,8 +5,31 @@ require 'optparse'
 require 'csv'
 
 DEFAULT_SCORE_MIN  = 15
-DEFAULT_SCORE_MEAN = 20
+DEFAULT_SCORE_MEAN = 16
 DEFAULT_MISMATCHES = 1
+
+def permutate(list, options = {})
+  permutations = options[:permutations] || 2
+  alphabet     = options[:alphabet]     || "ATCG"
+
+  permutations.times do
+    hash = list.inject({}) { |memo, obj| memo[obj.to_sym] = true; memo }
+
+    list.each do |word|
+      (0 ... word.size).each do |pos|
+        alphabet.each_char do |char|
+          new_word = word[0 ... pos] + char + word[ pos + 1 .. -1]
+
+          hash[new_word.to_sym] = true
+        end
+      end
+    end
+
+    list = hash.keys.map { |k| k.to_s }
+  end
+
+  list
+end
 
 ARGV << "-h" if ARGV.empty?
 
@@ -25,12 +48,12 @@ USAGE
     exit
   end
 
-  opts.on("-m", "--mapping_file <file>", String, "Path to mapping file") do |o|
-    options[:file_map] = o
+  opts.on("-m", "--samples_file <file>", String, "Path to mapping file") do |o|
+    options[:samples_file] = o
   end
 
-  opts.on("--mismatches <uint>", Integer, "Maximum mismatches allowed (default=1)") do |o|
-    options[:mismatches] = o
+  opts.on("--mismatches_max <uint>", Integer, "Maximum mismatches_max allowed (default=1)") do |o|
+    options[:mismatches_max] = o
   end
 
   opts.on("--scores_min <uint>", Integer, "Drop reads if a single position in the index have a quality score below scores_min (default=36)") do |o|
@@ -46,52 +69,74 @@ USAGE
   end
 end.parse!
 
-options[:mismatches] ||= DEFAULT_MISMATCHES
-options[:scores_min]  ||= DEFAULT_SCORE_MIN
-options[:scores_mean] ||= DEFAULT_SCORE_MEAN
+options[:mismatches_max] ||= DEFAULT_MISMATCHES
+options[:scores_min]     ||= DEFAULT_SCORE_MIN
+options[:scores_mean]    ||= DEFAULT_SCORE_MEAN
 
-raise OptionParser::MissingArgument, "No mapping_file specified."                              unless options[:file_map]
-raise OptionParser::InvalidArgument, "No such file: #{options[:file_map]}"                     unless File.file? options[:file_map]
-raise OptionParser::InvalidArgument, "mismatches must be >= 0 - not #{options[:mismatches]}"   unless options[:mismatches] >= 0
-raise OptionParser::InvalidArgument, "mismatches must be <= 3 - not #{options[:mismatches]}"   unless options[:mismatches] <= 3
-raise OptionParser::InvalidArgument, "scores_min must be >= 0 - not #{options[:scores_min]}"     unless options[:scores_min]  >= 0
-raise OptionParser::InvalidArgument, "scores_min must be <= 40 - not #{options[:scores_min]}"    unless options[:scores_min]  <= 40
-raise OptionParser::InvalidArgument, "scores_mean must be >= 0 - not #{options[:scores_mean]}"   unless options[:scores_mean] >= 0
-raise OptionParser::InvalidArgument, "scores_mean must be <= 40 - not #{options[:scores_mean]}"  unless options[:scores_mean] <= 40
+raise OptionParser::MissingArgument, "No samples_file specified."                                      unless options[:samples_file]
+raise OptionParser::InvalidArgument, "No such file: #{options[:samples_file]}"                         unless File.file? options[:samples_file]
+raise OptionParser::InvalidArgument, "mismatches_max must be >= 0 - not #{options[:mismatches_max]}"   unless options[:mismatches_max] >= 0
+raise OptionParser::InvalidArgument, "mismatches_max must be <= 3 - not #{options[:mismatches_max]}"   unless options[:mismatches_max] <= 3
+raise OptionParser::InvalidArgument, "scores_min must be >= 0 - not #{options[:scores_min]}"           unless options[:scores_min]     >= 0
+raise OptionParser::InvalidArgument, "scores_min must be <= 40 - not #{options[:scores_min]}"          unless options[:scores_min]     <= 40
+raise OptionParser::InvalidArgument, "scores_mean must be >= 0 - not #{options[:scores_mean]}"         unless options[:scores_mean]    >= 0
+raise OptionParser::InvalidArgument, "scores_mean must be <= 40 - not #{options[:scores_mean]}"        unless options[:scores_mean]    <= 40
 
 fastq_files = ARGV.dup
 
 raise ArgumentError, "Expected 4 input files - not #{fastq_files.size}" if fastq_files.size != 4
 
-samples = CSV.read(options[:file_map], col_sep: "\t")
+index1_file = fastq_files.grep(/_I1_/).first
+index2_file = fastq_files.grep(/_I2_/).first
+read1_file  = fastq_files.grep(/_R1_/).first
+read2_file  = fastq_files.grep(/_R2_/).first
+
+if read1_file =~ /.+(_S\d_L\d{3}_R1_\d{3}\.fastq.gz)$/
+  suffix1 = $1
+else
+  raise RunTimeError, "Unable to parse file suffix"
+end
+
+if read2_file =~ /.+(_S\d_L\d{3}_R2_\d{3}\.fastq.gz)$/
+  suffix2 = $1
+else
+  raise RunTimeError, "Unable to parse file suffix"
+end
+
+samples = CSV.read(options[:samples_file], col_sep: "\t")
 
 index_hash = {}
 file_hash  = {}
 
 samples.each do |sample|
-  index_hash["#{sample[1]}#{sample[2]}".to_sym] = sample[0].to_sym
-  file_forward = "#{sample[0]}_S0_L001_R1_001.fastq.gz"
-  file_reverse = "#{sample[0]}_S0_L001_R2_001.fastq.gz"
+  index_list1 = [sample[1]]
+  index_list2 = [sample[2]]
+
+  index_list1 = permutate(index_list1, permutations: options[:mismatches_max])
+  index_list2 = permutate(index_list2, permutations: options[:mismatches_max])
+
+  raise "Permutated list sizes differ: #{index_list1.size} != #{index_list2.size}" if index_list1.size != index_list2.size
+
+  index_list1.zip(index_list2).each.each do |index1, index2|
+    index_hash["#{index1}#{index2}".to_sym] = sample[0].to_sym
+  end
+
+  file_forward = "#{sample[0]}#{suffix1}"
+  file_reverse = "#{sample[0]}#{suffix2}"
   io_forward   = BioPieces::Fastq.open(file_forward, 'w', compress: :gzip)
   io_reverse   = BioPieces::Fastq.open(file_reverse, 'w', compress: :gzip)
   file_hash[sample[0].to_sym] = [io_forward, io_reverse]
 end
 
-file_forward = "Undertermined_R1.fastq.gz"
-file_reverse = "Undertermined_R2.fastq.gz"
+file_forward = "Undertermined#{suffix1}"
+file_reverse = "Undertermined#{suffix2}"
 io_forward   = BioPieces::Fastq.open(file_forward, 'w', compress: :gzip)
 io_reverse   = BioPieces::Fastq.open(file_reverse, 'w', compress: :gzip)
 file_hash[:undetermined] = [io_forward, io_reverse]
-
-index1_file = fastq_files.grep(/_I1_/).first
-index2_file = fastq_files.grep(/_I2_/).first
-read1_file  = fastq_files.grep(/_R1_/).first
-read2_file  = fastq_files.grep(/_R2_/).first
  
 stats = {
   count:           0,
-  match_hash:      0,
-  match_hamming:   0,
+  match:           0,
   undetermined:    0,
   index1_bad_mean: 0,
   index2_bad_mean: 0,
@@ -99,16 +144,7 @@ stats = {
   index2_bad_min:  0
 }
 
-def match_hamming(samples, mismatches, entry1, entry2)
-  samples.each do |sample|
-    if BioPieces::Hamming.distance(sample[1], entry1.seq) <= mismatches and
-       BioPieces::Hamming.distance(sample[2], entry2.seq) <= mismatches
-      return sample[0].to_sym
-    end
-  end
-
-  nil
-end
+time_start = Time.now
 
 begin
   i1_io = BioPieces::Fastq.open(index1_file)
@@ -120,41 +156,41 @@ begin
 
   while i1 = i1_io.get_entry and i2 = i2_io.get_entry and r1 = r1_io.get_entry and r2 = r2_io.get_entry
     if i1.scores_mean < options[:scores_mean]
-      stats[:index1_bad_mean] += 1
-      stats[:undetermined] += 1
+      stats[:index1_bad_mean] += 2
+      stats[:undetermined] += 2
       io_forward, io_reverse = file_hash[:undetermined]
     elsif i2.scores_mean < options[:scores_mean]
-      stats[:index2_bad_mean] += 1
-      stats[:undetermined] += 1
+      stats[:index2_bad_mean] += 2
+      stats[:undetermined] += 2
       io_forward, io_reverse = file_hash[:undetermined]
     elsif i1.scores_min < options[:scores_min]
-      stats[:index1_bad_min] += 1
-      stats[:undetermined] += 1
+      stats[:index1_bad_min] += 2
+      stats[:undetermined] += 2
       io_forward, io_reverse = file_hash[:undetermined]
     elsif i2.scores_min < options[:scores_min]
-      stats[:index2_bad_min] += 1
-      stats[:undetermined] += 1
+      stats[:index2_bad_min] += 2
+      stats[:undetermined] += 2
       io_forward, io_reverse = file_hash[:undetermined]
     elsif sample_id = index_hash["#{i1.seq}#{i2.seq}".to_sym]
-      stats[:match_hash] += 1
-      io_forward, io_reverse = file_hash[sample_id]
-    elsif sample_id = match_hamming(samples, options[:mismatches], i1, i2)
-      stats[:match_hamming] += 1
+      stats[:match] += 2
       io_forward, io_reverse = file_hash[sample_id]
     else
-      stats[:undetermined] += 1
+      stats[:undetermined] += 2
       io_forward, io_reverse = file_hash[:undetermined]
     end
 
     io_forward.puts r1.to_fastq
     io_reverse.puts r2.to_fastq
 
-    stats[:count] += 1
+    stats[:count] += 2
 
-    if options[:verbose] and (stats[:count] % 10_000) == 0
+    if options[:verbose] and (stats[:count] % 1_000) == 0
       print "\e[1;1H"    # Console code to move cursor to 1,1 coordinate.
+      stats[:time] = (Time.mktime(0) + (Time.now - time_start)).strftime("%H:%M:%S")
       pp stats
     end
+
+    break if stats[:count] == 1_000_000
   end
 ensure
   i1_io.close
