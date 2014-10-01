184c1b080de2a7c20e044012ff1bb84169753e55
Martin Asser Hansen <mail@maasha.dk>
2014-10-01 11:43:17 +0200

    put all relevant code in a class

--- miseq_demultiplexer.rb
+++ miseq_demultiplexer.rb
@@ -5,6 +5,8 @@ require 'optparse'
 require 'csv'
 require 'google_hash'
 
+DEBUG = 100_000
+
 USAGE = <<USAGE
   This program demultiplexes Illumina Paired data given a samples file and four
   FASTQ files containing forward and reverse index data and forward and reverse
@@ -55,59 +57,238 @@ USAGE = <<USAGE
   Options:
 USAGE
 
-DEFAULT_SCORE_MIN  = 15
-DEFAULT_SCORE_MEAN = 16
-DEFAULT_MISMATCHES = 1
+class Demultiplexer
+  INDEX_ID = 0
+  INDEX1   = 1
+  INDEX2   = 2
 
-INDEX_ID = 0
-INDEX1   = 1
-INDEX2   = 2
-
-def suffix_extract(file, options)
-  if file =~ /.+(_S\d_L\d{3}_R[12]_\d{3}).+$/
-    suffix = $1
-  else
-    raise RuntimeError, "Unable to parse file suffix from: #{file}"
+  def self.run(fastq_files, options)
+    d = self.new(fastq_files, options)
+    d.demultiplex
   end
 
-  case options[:compress]
-  when /gzip/
-    suffix << ".fastq.gz"
-  when /bzip2/
-    suffix << ".fastq.bz2"
-  else
-    suffix << ".fastq"
+  def initialize(fastq_files = [], options = {})
+    @fastq_files  = fastq_files
+    @options      = options
+    @index1_file  = nil
+    @index2_file  = nil
+    @read1_file   = nil
+    @read2_file   = nil
+    @suffix1      = nil
+    @suffix2      = nil
+    @samples      = nil
+    @undetermined = nil
+    @stats        = nil
+    @miss_hash    = nil
+    @file_hash    = nil
+    @index_hash   = nil
   end
 
-  suffix
-end
+  def suffix_extract(file)
+    if file =~ /.+(_S\d_L\d{3}_R[12]_\d{3}).+$/
+      suffix = $1
+    else
+      raise RuntimeError, "Unable to parse file suffix from: #{file}"
+    end
 
-def hash_index(index)
-  index.tr("ATCG", "0123").to_i
-end
+    case @options[:compress]
+    when /gzip/
+      suffix << ".fastq.gz"
+    when /bzip2/
+      suffix << ".fastq.bz2"
+    else
+      suffix << ".fastq"
+    end
 
-def permutate(list, options = {})
-  permutations = options[:permutations] || 2
-  alphabet     = options[:alphabet]     || "ATCG"
+    suffix
+  end
 
-  permutations.times do
-    hash = list.inject({}) { |memo, obj| memo[obj.to_sym] = true; memo }
+  def files_open
+    file_hash  = {}
 
-    list.each do |word|
-      (0 ... word.size).each do |pos|
-        alphabet.each_char do |char|
-          new_word = "#{word[0 ... pos]}#{char}#{word[pos + 1 .. -1]}"
+    @samples.each_with_index do |sample, i|
+      file_forward = "#{sample[INDEX_ID]}#{@suffix1}"
+      file_reverse = "#{sample[INDEX_ID]}#{@suffix2}"
+      io_forward   = BioPieces::Fastq.open(File.join(@options[:output_dir], file_forward), 'w', compress: @options[:compress])
+      io_reverse   = BioPieces::Fastq.open(File.join(@options[:output_dir], file_reverse), 'w', compress: @options[:compress])
+      file_hash[i] = [io_forward, io_reverse]
+    end
 
-          hash[new_word.to_sym] = true
-        end
+    @undetermined = @samples.size + 1
+
+    file_forward             = "Undetermined#{@suffix1}"
+    file_reverse             = "Undetermined#{@suffix2}"
+    io_forward               = BioPieces::Fastq.open(File.join(@options[:output_dir], file_forward), 'w', compress: @options[:compress])
+    io_reverse               = BioPieces::Fastq.open(File.join(@options[:output_dir], file_reverse), 'w', compress: @options[:compress])
+    file_hash[@undetermined] = [io_forward, io_reverse]
+
+    at_exit { file_hash.each_value { |value| value[0].close; value[1].close } }
+
+    file_hash
+  end
+
+  def index_create
+    index_hash = (@options[:mismatches_max] <= 1) ? GoogleHashSparseLongToInt.new : GoogleHashDenseLongToInt.new
+
+    @samples.each_with_index do |sample, i|
+      index_list1 = [sample[INDEX1]]
+      index_list2 = [sample[INDEX2]]
+
+      index_list1 = permutate(index_list1, permutations: @options[:mismatches_max])
+      index_list2 = permutate(index_list2, permutations: @options[:mismatches_max])
+
+      raise "Permutated list sizes differ: #{index_list1.size} != #{index_list2.size}" if index_list1.size != index_list2.size
+
+      index_list1.product(index_list2).each do |index1, index2|
+        index_hash[hash_index("#{index1}#{index2}")] = i
       end
     end
 
-    list = hash.keys.map { |k| k.to_s }
+    index_hash
   end
 
-  list
+  def permutate(list, options = {})
+    permutations = options[:permutations] || 2
+    alphabet     = options[:alphabet]     || "ATCG"
+
+    permutations.times do
+      hash = list.inject({}) { |memo, obj| memo[obj.to_sym] = true; memo }
+
+      list.each do |word|
+        (0 ... word.size).each do |pos|
+          alphabet.each_char do |char|
+            new_word = "#{word[0 ... pos]}#{char}#{word[pos + 1 .. -1]}"
+
+            hash[new_word.to_sym] = true
+          end
+        end
+      end
+
+      list = hash.keys.map { |k| k.to_s }
+    end
+
+    list
+  end
+
+  def hash_index(index)
+    index.tr("ATCG", "0123").to_i
+  end
+
+  def demultiplex
+    @index1_file = @fastq_files.grep(/_I1_/).first
+    @index2_file = @fastq_files.grep(/_I2_/).first
+    @read1_file  = @fastq_files.grep(/_R1_/).first
+    @read2_file  = @fastq_files.grep(/_R2_/).first
+    @suffix1     = suffix_extract(@read1_file)
+    @suffix2     = suffix_extract(@read2_file)
+    @samples     = CSV.read(@options[:samples_file], col_sep: "\t")
+    @file_hash   = files_open
+    @index_hash  = index_create
+
+    @stats = {
+      count:           0,
+      match:           0,
+      undetermined:    0,
+      index1_bad_mean: 0,
+      index2_bad_mean: 0,
+      index1_bad_min:  0,
+      index2_bad_min:  0
+    }
+
+    @miss_hash = {
+      forward: Hash.new(0),
+      reverse: Hash.new(0)
+    }
+
+    time_start = Time.now
+
+    begin
+      i1_io = BioPieces::Fastq.open(@index1_file)
+      i2_io = BioPieces::Fastq.open(@index2_file)
+      r1_io = BioPieces::Fastq.open(@read1_file)
+      r2_io = BioPieces::Fastq.open(@read2_file)
+
+      print "\e[H\e[2J" if @options[:verbose] # Console code to clear screen
+
+      while i1 = i1_io.get_entry and i2 = i2_io.get_entry and r1 = r1_io.get_entry and r2 = r2_io.get_entry
+        if i1.scores_mean < @options[:scores_mean]
+          @stats[:index1_bad_mean] += 2
+          @stats[:undetermined] += 2
+          io_forward, io_reverse = @file_hash[@undetermined]
+        elsif i2.scores_mean < @options[:scores_mean]
+          @stats[:index2_bad_mean] += 2
+          @stats[:undetermined] += 2
+          io_forward, io_reverse = @file_hash[@undetermined]
+        elsif i1.scores_min < @options[:scores_min]
+          @stats[:index1_bad_min] += 2
+          @stats[:undetermined] += 2
+          io_forward, io_reverse = @file_hash[@undetermined]
+        elsif i2.scores_min < @options[:scores_min]
+          @stats[:index2_bad_min] += 2
+          @stats[:undetermined] += 2
+          io_forward, io_reverse = @file_hash[@undetermined]
+        elsif sample_id = @index_hash[hash_index("#{i1.seq}#{i2.seq}")]
+          @stats[:match] += 2
+          io_forward, io_reverse = @file_hash[sample_id]
+        else
+          @stats[:undetermined] += 2
+          io_forward, io_reverse = @file_hash[@undetermined]
+
+          @miss_hash[:forward][i1.seq] += 1
+          @miss_hash[:reverse][i2.seq] += 1
+        end
+
+        io_forward.puts r1.to_fastq
+        io_reverse.puts r2.to_fastq
+
+        @stats[:count] += 2
+
+        if @options[:verbose] and (@stats[:count] % 1_000) == 0
+          print "\e[1;1H"    # Console code to move cursor to 1,1 coordinate.
+          @stats[:time] = (Time.mktime(0) + (Time.now - time_start)).strftime("%H:%M:%S")
+          pp @stats
+        end
+
+        break if DEBUG and @stats[:count] == DEBUG
+      end
+    ensure
+      i1_io.close
+      i2_io.close
+      r1_io.close
+      r2_io.close
+    end
+
+    pp @stats if @options[:verbose]
+
+    save_undetermined
+    save_log
+  end
+
+  def save_undetermined
+    @samples.each do |sample|
+      @miss_hash[:forward].delete sample[INDEX1]
+      @miss_hash[:reverse].delete sample[INDEX2]
+    end
+
+    File.open(File.join(@options[:output_dir], "Undetermined_forward.tsv"), 'w') do |ios|
+      @miss_hash[:forward].sort_by { |index, count| -1 * count }.each { |index, count| ios.puts "#{count}\t#{index}" }
+    end
+
+    File.open(File.join(@options[:output_dir], "Undetermined_reverse.tsv"), 'w') do |ios|
+      @miss_hash[:reverse].sort_by { |index, count| -1 * count }.each { |index, count| ios.puts "#{count}\t#{index}" }
+    end
+  end
+
+  def save_log
+    File.open(File.join(@options[:output_dir], "Demultiplex.log"), 'w') do |ios|
+      PP.pp(@stats, ios)
+    end
+  end
 end
+
+DEFAULT_SCORE_MIN  = 15
+DEFAULT_SCORE_MEAN = 16
+DEFAULT_MISMATCHES = 1
 
 ARGV << "-h" if ARGV.empty?
 
@@ -121,11 +302,11 @@ OptionParser.new do |opts|
     exit
   end
 
-  opts.on("-m", "--samples_file <file>", String, "Path to mapping file") do |o|
+  opts.on("-s", "--samples_file <file>", String, "Path to samples file") do |o|
     options[:samples_file] = o
   end
 
-  opts.on("--mismatches_max <uint>", Integer, "Maximum mismatches_max allowed (default=#{DEFAULT_MISMATCHES})") do |o|
+  opts.on("-m", "--mismatches_max <uint>", Integer, "Maximum mismatches_max allowed (default=#{DEFAULT_MISMATCHES})") do |o|
     options[:mismatches_max] = o
   end
 
@@ -176,138 +357,4 @@ fastq_files = ARGV.dup
 
 raise ArgumentError, "Expected 4 input files - not #{fastq_files.size}" if fastq_files.size != 4
 
-index1_file = fastq_files.grep(/_I1_/).first
-index2_file = fastq_files.grep(/_I2_/).first
-read1_file  = fastq_files.grep(/_R1_/).first
-read2_file  = fastq_files.grep(/_R2_/).first
-suffix1     = suffix_extract(read1_file, options)
-suffix2     = suffix_extract(read2_file, options)
-samples     = CSV.read(options[:samples_file], col_sep: "\t")
-
-if options[:mismatches_max] <= 1
-  index_hash = GoogleHashSparseLongToInt.new
-else
-  index_hash = GoogleHashDenseLongToInt.new
-end
-
-file_hash  = {}
-
-samples.each_with_index do |sample, i|
-  index_list1 = [sample[INDEX1]]
-  index_list2 = [sample[INDEX2]]
-
-  index_list1 = permutate(index_list1, permutations: options[:mismatches_max])
-  index_list2 = permutate(index_list2, permutations: options[:mismatches_max])
-
-  raise "Permutated list sizes differ: #{index_list1.size} != #{index_list2.size}" if index_list1.size != index_list2.size
-
-  index_list1.product(index_list2).each do |index1, index2|
-    index_hash[hash_index("#{index1}#{index2}")] = i
-  end
-
-  file_forward = "#{sample[INDEX_ID]}#{suffix1}"
-  file_reverse = "#{sample[INDEX_ID]}#{suffix2}"
-  io_forward   = BioPieces::Fastq.open(File.join(options[:output_dir], file_forward), 'w', compress: options[:compress])
-  io_reverse   = BioPieces::Fastq.open(File.join(options[:output_dir], file_reverse), 'w', compress: options[:compress])
-  file_hash[i] = [io_forward, io_reverse]
-end
-
-undetermined = samples.size + 1
-
-file_forward = "Undetermined#{suffix1}"
-file_reverse = "Undetermined#{suffix2}"
-io_forward   = BioPieces::Fastq.open(File.join(options[:output_dir], file_forward), 'w', compress: options[:compress])
-io_reverse   = BioPieces::Fastq.open(File.join(options[:output_dir], file_reverse), 'w', compress: options[:compress])
-file_hash[undetermined] = [io_forward, io_reverse]
- 
-stats = {
-  count:           0,
-  match:           0,
-  undetermined:    0,
-  index1_bad_mean: 0,
-  index2_bad_mean: 0,
-  index1_bad_min:  0,
-  index2_bad_min:  0
-}
-
-miss_hash = {
-  forward: Hash.new(0),
-  reverse: Hash.new(0)
-}
-
-time_start = Time.now
-
-begin
-  i1_io = BioPieces::Fastq.open(index1_file)
-  i2_io = BioPieces::Fastq.open(index2_file)
-  r1_io = BioPieces::Fastq.open(read1_file)
-  r2_io = BioPieces::Fastq.open(read2_file)
-
-  print "\e[H\e[2J" if options[:verbose] # Console code to clear screen
-
-  while i1 = i1_io.get_entry and i2 = i2_io.get_entry and r1 = r1_io.get_entry and r2 = r2_io.get_entry
-    if i1.scores_mean < options[:scores_mean]
-      stats[:index1_bad_mean] += 2
-      stats[:undetermined] += 2
-      io_forward, io_reverse = file_hash[undetermined]
-    elsif i2.scores_mean < options[:scores_mean]
-      stats[:index2_bad_mean] += 2
-      stats[:undetermined] += 2
-      io_forward, io_reverse = file_hash[undetermined]
-    elsif i1.scores_min < options[:scores_min]
-      stats[:index1_bad_min] += 2
-      stats[:undetermined] += 2
-      io_forward, io_reverse = file_hash[undetermined]
-    elsif i2.scores_min < options[:scores_min]
-      stats[:index2_bad_min] += 2
-      stats[:undetermined] += 2
-      io_forward, io_reverse = file_hash[undetermined]
-    elsif sample_id = index_hash[hash_index("#{i1.seq}#{i2.seq}")]
-      stats[:match] += 2
-      io_forward, io_reverse = file_hash[sample_id]
-    else
-      stats[:undetermined] += 2
-      io_forward, io_reverse = file_hash[undetermined]
-
-      miss_hash[:forward][i1.seq] += 1
-      miss_hash[:reverse][i2.seq] += 1
-    end
-
-    io_forward.puts r1.to_fastq
-    io_reverse.puts r2.to_fastq
-
-    stats[:count] += 2
-
-    if options[:verbose] and (stats[:count] % 1_000) == 0
-      print "\e[1;1H"    # Console code to move cursor to 1,1 coordinate.
-      stats[:time] = (Time.mktime(0) + (Time.now - time_start)).strftime("%H:%M:%S")
-      pp stats
-    end
-  end
-ensure
-  i1_io.close
-  i2_io.close
-  r1_io.close
-  r2_io.close
-end
-
-pp stats if options[:verbose]
-
-samples.each do |sample|
-  miss_hash[:forward].delete sample[INDEX1]
-  miss_hash[:reverse].delete sample[INDEX2]
-end
-
-File.open(File.join(options[:output_dir], "Undetermined_forward.tsv"), 'w') do |ios|
-  miss_hash[:forward].sort_by { |index, count| -1 * count }.each { |index, count| ios.puts "#{count}\t#{index}" }
-end
-
-File.open(File.join(options[:output_dir], "Undetermined_reverse.tsv"), 'w') do |ios|
-  miss_hash[:reverse].sort_by { |index, count| -1 * count }.each { |index, count| ios.puts "#{count}\t#{index}" }
-end
-
-File.open(File.join(options[:output_dir], "Demultiplex.log"), 'w') do |ios|
-  PP.pp(stats, ios)
-end
-
-at_exit { file_hash.each_value { |value| value[0].close; value[1].close } }
+Demultiplexer.run(fastq_files, options)
