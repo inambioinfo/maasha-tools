b8d1eb6dc0e901f705d4573c927b429029ea7ec0
Martin Asser Hansen <mail@maasha.dk>
2015-08-13 15:05:47 +0200

    Finished Data tests

--- MiSeq/test/test_miseq.rb
+++ MiSeq/test/test_miseq.rb
@@ -3,16 +3,63 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..')
 require 'test/helper'
 require 'lib/miseq'
 
+# rubocop:disable ClassLength
+
 # Test class for MiSeq.
 class TestMiSeq < Test::Unit::TestCase
   def setup
-    @dir          = Dir.mktmpdir('miseq')
-    @file_stats   = File.join(@dir, 'GenerateFASTQRunStatistics.xml')
-    @file_samples = File.join(@dir, 'Samplesheet.csv')
+    @dir_src      = Dir.mktmpdir('miseq_src')
+    @dir_dst      = Dir.mktmpdir('miseq_dst')
+    @file_stats   = File.join(@dir_src, 'GenerateFASTQRunStatistics.xml')
+    @file_samples = File.join(@dir_src, 'Samplesheet.csv')
+
+    setup_dir_src_ok
+    setup_dir_src_unfinished
+  end
+
+  def setup_dir_src_ok
+    @dir_src_ok = Dir.mktmpdir('miseq_src_ok')
+
+    3.times do |i|
+      data_dir     = File.join(@dir_src_ok, ('130400'.to_i + i).to_s + '_test')
+      file_stats   = File.join(data_dir, 'GenerateFASTQRunStatistics.xml')
+      file_samples = File.join(data_dir, 'SampleSheet.csv')
+
+      Dir.mkdir(data_dir)
+
+      File.open(file_stats, 'w') { |ios| ios.puts "  <CompletionTime>#{i}" }
+
+      File.open(file_samples, 'w') do |ios|
+        ios.puts "Investigator Name, Martin Hansen #{i}"
+        ios.puts "Experiment Name, Big Bang #{i}"
+      end
+    end
+  end
+
+  def setup_dir_src_unfinished
+    @dir_src_unfinished = Dir.mktmpdir('miseq_src_unfinished')
+
+    3.times do |i|
+      data_dir     = File.join(@dir_src_unfinished,
+                               ('130400'.to_i + i).to_s + '_test')
+      file_stats   = File.join(data_dir, 'GenerateFASTQRunStatistics.xml')
+      file_samples = File.join(data_dir, 'SampleSheet.csv')
+
+      Dir.mkdir(data_dir)
+
+      if i != 2
+        File.open(file_stats, 'w') { |ios| ios.puts "  <CompletionTime>#{i}" }
+      end
+
+      File.open(file_samples, 'w') do |ios|
+        ios.puts "Investigator Name, Martin Hansen #{i}"
+        ios.puts "Experiment Name, Big Bang #{i}"
+      end
+    end
   end
 
   def teardown
-    FileUtils.rm_rf @dir
+    FileUtils.rm_rf @dir_src
   end
 
   test 'RunStatistics#complete? with non existing file' do
@@ -89,21 +136,54 @@ class TestMiSeq < Test::Unit::TestCase
 
   test 'DataDir#rename with existing dir fails' do
     dd       = MiSeq::DataDir.new('/MiSeq/131223_')
-    new_name = File.join(@dir, 'new')
+    new_name = File.join(@dir_src, 'new')
     Dir.mkdir(new_name)
     assert_raise(MiSeq::DataDirError) { dd.rename(new_name) }
   end
 
   test 'DataDir#rename works OK' do
-    old_name = File.join(@dir, '131223_')
-    new_name = File.join(@dir, 'new')
+    old_name = File.join(@dir_src, '131223_')
+    new_name = File.join(@dir_src, 'new')
     dd       = MiSeq::DataDir.new(old_name)
     Dir.mkdir(old_name)
     dd.rename(new_name)
     assert_true(File.directory? dd.dir)
   end
 
-  # test 'Data#' do
-  #   assert_equal(1, 2)
-  # end
+  test 'Data#sync works OK' do
+    MiSeq::Data.sync(@dir_src_ok, @dir_dst)
+
+    file1 = '2013-04-00_Martin_Hansen_0_Big_Bang_0.tar'
+    file2 = '2013-04-01_Martin_Hansen_1_Big_Bang_1.tar'
+    file3 = '2013-04-02_Martin_Hansen_2_Big_Bang_2.tar'
+
+    assert_true(File.exist? File.join(@dir_dst, file1))
+    assert_true(File.exist? File.join(@dir_dst, file2))
+    assert_true(File.exist? File.join(@dir_dst, file3))
+  end
+
+  test 'Data#sync with existing dirs works OK' do
+    MiSeq::Data.sync(@dir_src_ok, @dir_dst)
+    MiSeq::Data.sync(@dir_src_ok, @dir_dst)
+
+    file1 = '2013-04-00_Martin_Hansen_0_Big_Bang_0.tar'
+    file2 = '2013-04-01_Martin_Hansen_1_Big_Bang_1.tar'
+    file3 = '2013-04-02_Martin_Hansen_2_Big_Bang_2.tar'
+
+    assert_true(File.exist? File.join(@dir_dst, file1))
+    assert_true(File.exist? File.join(@dir_dst, file2))
+    assert_true(File.exist? File.join(@dir_dst, file3))
+  end
+
+  test 'Data#sync with unfinished data dir works OK' do
+    MiSeq::Data.sync(@dir_src_unfinished, @dir_dst)
+
+    file1 = '2013-04-00_Martin_Hansen_0_Big_Bang_0.tar'
+    file2 = '2013-04-01_Martin_Hansen_1_Big_Bang_1.tar'
+    file3 = '2013-04-02_Martin_Hansen_2_Big_Bang_2.tar'
+
+    assert_true(File.exist? File.join(@dir_dst, file1))
+    assert_true(File.exist? File.join(@dir_dst, file2))
+    assert_false(File.exist? File.join(@dir_dst, file3))
+  end
 end
