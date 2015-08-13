5c32d4ad71db69599f8e5130d29c92afd04ed656
Martin Asser Hansen <mail@maasha.dk>
2015-08-13 11:48:08 +0200

    Fixed test helper

--- MiSeq/test/helper.rb
+++ MiSeq/test/helper.rb
@@ -3,13 +3,17 @@ require 'tempfile'
 require 'fileutils'
 require 'test/unit'
 
-# Appending Test::Unit::TestCase class.
-class Test::Unit::TestCase
-  # Monkey patch of TestCase to define test method.
-  #
-  # @param desc [String] Test description.
-  # @param impl [Proc]   Code block.
-  def self.test(desc, &impl)
-    define_method("test #{desc}", &impl)
+module Test
+  module Unit
+    # Appending TestCase class.
+    class TestCase
+      # Monkey patch of TestCase to define test method.
+      #
+      # @param desc [String] Test description.
+      # @param impl [Proc]   Code block.
+      def self.test(desc, &impl)
+        define_method("test #{desc}", &impl)
+      end
+    end
   end
 end
