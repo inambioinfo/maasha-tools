c5a9297675ecd9e12511b8ac1e2fc45e71ab11d7
Martin Asser Hansen <mail@maasha.dk>
2015-08-13 11:40:22 +0200

    Test helper in place

--- MiSeq/test/helper.rb
+++ MiSeq/test/helper.rb
@@ -3,7 +3,12 @@ require 'tempfile'
 require 'fileutils'
 require 'test/unit'
 
+# Appending Test::Unit::TestCase class.
 class Test::Unit::TestCase
+  # Monkey patch of TestCase to define test method.
+  #
+  # @param desc [String] Test description.
+  # @param impl [Proc]   Code block.
   def self.test(desc, &impl)
     define_method("test #{desc}", &impl)
   end
